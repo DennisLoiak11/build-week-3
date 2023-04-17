c13328de13b8af0f11828799f4f2daaae70ff070
Stefano-Paul-Pascu <stefanopascu@gmail.com>
2023-04-17 17:00:16 +0200

    first commit

---
+++ src/components/AsideDx.jsx
@@ -0,0 +1,63 @@
+import { Component } from "react";
+import "../App.css";
+
+const BASE_URL = "https://striveschool-api.herokuapp.com/api/profile/";
+
+function AsideDx(){
+    return(
+        <div className="col-3 border rounded">
+            <h2 className="m-3">Altri profili consultati</h2>
+            <div className="m-3">
+                <div className="mb-1 mt-1">
+                    <h3>Paolo Stella</h3>
+                    <p className="text-secondary">Imprenditore</p>
+                    <button className="btn border border-2 text-secondary">Messaggio</button>
+                </div>
+                <hr />
+                <div className="mb-1 mt-1">
+                    <h3>Paolo Stella</h3>
+                    <p className="text-secondary">Imprenditore</p>
+                    <button className="btn border border-2 text-secondary">Messaggio</button>
+                </div>
+                <hr />
+                <div className="mb-1 mt-1">
+                    <h3>Paolo Stella</h3>
+                    <p className="text-secondary">Imprenditore</p>
+                    <button className="btn border border-2 text-secondary">Messaggio</button>
+                </div>
+                <hr />
+                <div className="mb-1 mt-1">
+                    <h3>Paolo Stella</h3>
+                    <p className="text-secondary">Imprenditore</p>
+                    <button className="btn border border-2 text-secondary">Messaggio</button>
+                </div>
+                <hr />
+                <div className="mb-1 mt-1">
+                    <h3>Paolo Stella</h3>
+                    <p className="text-secondary">Imprenditore</p>
+                    <button className="btn border border-2 text-secondary">Messaggio</button>
+                </div>
+                <hr />
+                <div className="mb-1 mt-1">
+                    <h3>Paolo Stella</h3>
+                    <p className="text-secondary">Imprenditore</p>
+                    <button className="btn border border-2 text-secondary">Messaggio</button>
+                </div>
+                <hr />
+                <div className="mb-1 mt-1">
+                    <h3>Paolo Stella</h3>
+                    <p className="text-secondary">Imprenditore</p>
+                    <button className="btn border border-2 text-secondary">Messaggio</button>
+                </div>
+                <hr />
+                <div className="mb-1 mt-1">
+                    <h3>Paolo Stella</h3>
+                    <p className="text-secondary">Imprenditore</p>
+                    <button className="btn border border-2 text-secondary">Messaggio</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AsideDx;
