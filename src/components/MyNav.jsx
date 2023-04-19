791626acb681205fc7f343f61d4de1b7c1ff69c9
Pierluigi Monzon <pierlui-gi@hotmail.com>
2023-04-19 09:55:37 +0200

    .

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Logo from "../img/logo_linkedin.png";
+import Logo from "../img/logo_linkedIn.png";
 import InputGroup from "react-bootstrap/InputGroup";
 import Form from "react-bootstrap/Form";
 import { FaHome } from "react-icons/fa";
