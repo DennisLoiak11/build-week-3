780ac9ecb1b4be89d52958fcbb56dd7eb753858b
Stefano-Paul-Pascu <stefanopascu@gmail.com>
2023-04-20 15:18:57 +0200

    commit prova

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AsideDx1 from './components/AsideDx-1';
+import AsideSx1 from './components/AsideSx-1';
 
 function App() {
     return (
         <>
+        <AsideSx1/>
         <AsideDx1 />
         </>
     );
--- src/components/AsideSx-1.jsx
+++ src/components/AsideSx-1.jsx
@@ -1,3 +1,12 @@
 import { useState, useEffect } from "react";
+import gerry from '../gerrys/gerry.jpg'
+import gerry2 from '../gerrys/gerry2.jpg'
 import '../App.css';
 
+function AsideSx1(){
+    return(
+        
+    )
+}
+
+export default AsideSx1;
